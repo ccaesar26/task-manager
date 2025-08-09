17769a651d86ac934bcd8dee3f4046a807704ef6
Cezar Constăndoiu <93541398+ccaesar26@users.noreply.github.com>
2025-08-09 19:44:26 +0300

    Add repository layer

---
+++ src/main/java/com/is/lab/taskmanager/repository/ProjectRepository.java
@@ -0,0 +1,9 @@
+package com.is.lab.taskmanager.repository;
+
+import com.is.lab.taskmanager.model.Project;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends JpaRepository<Project, Long> {
+}
---
+++ src/main/java/com/is/lab/taskmanager/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.is.lab.taskmanager.repository;
+
+import com.is.lab.taskmanager.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
